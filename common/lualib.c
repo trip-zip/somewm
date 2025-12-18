8a9014e268b551d8bf1107bf1a1f8ebd69b256d1
Jimmy Cozza <moatcozza@gmail.com>
2025-12-18 14:51:08 -0700

    fix __gc ordering bug causing use-after-free during GC
    luaA_class_setup() was setting __gc before applying the meta table,
    allowing custom __gc handlers (in drawable, drawin, screen, timer,
    selection_* classes) to overwrite luaA_class_gc.
    
    This caused luaA_class_gc's critical cleanup to be skipped:
    - Metatable wasn't replaced with "invalid" marker
    - Other finalizers in the same GC cycle could access freed objects
    - Memory corruption and crashes in LuaJIT finalizers
    
    Fix: Apply meta table FIRST, then set __gc, so luaA_class_gc always
    handles cleanup properly.
    
    Also adds:
    - ASAN build support (ASAN=1 flag, config.local.mk for local overrides)
    - Defensive improvements to luaA_dumpstack and die()
    - Version bump to 0.2-dev

--- common/lualib.c
+++ common/lualib.c
@@ -19,6 +19,7 @@
 
 #include "common/lualib.h"
 #include "objects/luaa.h"
+#include <string.h>
 
 lua_CFunction lualib_dofunction_on_error;
 
@@ -36,14 +37,43 @@ void luaA_checktable(lua_State *L, int idx)
 
 void luaA_dumpstack(lua_State *L)
 {
-    fprintf(stderr, "-------- Lua stack dump ---------\n");
-    for(int i = lua_gettop(L); i; i--)
+    int top;
+
+    if (!L) {
+        fprintf(stderr, "-------- Lua stack dump: NULL state! ---------\n");
+        return;
+    }
+
+    top = lua_gettop(L);
+
+    /* Sanity check - stack should never be this large */
+    if (top < 0 || top > 10000) {
+        fprintf(stderr, "-------- Lua stack dump: CORRUPTED (top=%d) ---------\n", top);
+        return;
+    }
+
+    fprintf(stderr, "-------- Lua stack dump (top=%d) ---------\n", top);
+    for(int i = top; i >= 1; i--)
     {
         int t = lua_type(L, i);
+
+        /* Sanity check type value */
+        if (t < LUA_TNONE || t > LUA_TTHREAD) {
+            fprintf(stderr, "%d: CORRUPTED TYPE (%d)\n", i, t);
+            continue;
+        }
+
         switch (t)
         {
           case LUA_TSTRING:
-            fprintf(stderr, "%d: string: `%s'\n", i, lua_tostring(L, i));
+            {
+                const char *s = lua_tostring(L, i);
+                if (s)
+                    fprintf(stderr, "%d: string: `%.100s'%s\n", i, s,
+                            strlen(s) > 100 ? "..." : "");
+                else
+                    fprintf(stderr, "%d: string: (null)\n", i);
+            }
             break;
           case LUA_TBOOLEAN:
             fprintf(stderr, "%d: bool:   %s\n", i, lua_toboolean(L, i) ? "true" : "false");
@@ -54,9 +84,16 @@ void luaA_dumpstack(lua_State *L)
           case LUA_TNIL:
             fprintf(stderr, "%d: nil\n", i);
             break;
-          default:
+          case LUA_TTABLE:
+          case LUA_TUSERDATA:
+            /* Only call luaA_rawlen for types that support it safely */
             fprintf(stderr, "%d: %s\t#%d\t%p\n", i, lua_typename(L, t),
                     (int) luaA_rawlen(L, i),
+                    lua_topointer(L, i));
+            break;
+          default:
+            /* For other types (function, thread, lightuserdata), don't call rawlen */
+            fprintf(stderr, "%d: %s\t%p\n", i, lua_typename(L, t),
                     lua_topointer(L, i));
             break;
         }
