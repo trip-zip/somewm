75d475250d8a58618b19837829fcdb0ccfe7ac19
Jimmy Cozza <moatcozza@gmail.com>
2026-01-05 21:09:56 -0700

    refactor: merge objects/awesome.c into luaa.c
    Merge the awesome module code from objects/awesome.c into luaa.c
    to match AwesomeWM's file organization where the awesome global
    is defined alongside other Lua infrastructure.
    
    Changes:
    - Move all awesome module functions to luaa.c
    - Add luaA_awesome_setup/set_conffile declarations to luaa.h
    - Remove objects/awesome.c and objects/awesome.h
    - Fix include order (draw.h before globalconf.h)
    
    Relocations remaining: 16 → 11

--- luaa.h
+++ luaa.h
@@ -80,6 +80,10 @@ void luaA_init(void);
 void luaA_loadrc(void);
 void luaA_cleanup(void);
 
+/* awesome module (merged from objects/awesome.c) */
+void luaA_awesome_setup(lua_State *L);
+void luaA_awesome_set_conffile(lua_State *L, const char *conffile);
+
 /* Config scanner (somewm --check) */
 int luaA_check_config(const char *config_path, bool use_color);
 
