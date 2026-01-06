982c369d6653b752146eb5f8d969e419a55a7bdc
Jimmy Cozza <moatcozza@gmail.com>
2026-01-06 14:59:05 -0700

    api: align mouse.c symbols with AwesomeWM
    Add missing AwesomeWM functions:
    - mouse_query_pointer() - Wayland implementation
    - mouse_query_pointer_root() - wrapper for above
    - mouse_warp_pointer() - Wayland cursor positioning
    - luaA_mouse_pushstatus() - push mouse status table to Lua
    
    Fix luaA_mouse_setup() to use luaA_openlib() like AwesomeWM does.
    The old manual metatable setup didn't set metatable.__index = metatable,
    which broke method lookup (mouse.coords, etc).
    
    Update luaA_mouse_index/newindex to match AwesomeWM patterns.

--- mouse.c
+++ mouse.c
@@ -1,251 +1,321 @@
 /*
- * mouse.c - Mouse object bindings for Lua
- * Provides the global 'mouse' object with coordinate and detection functions
+ * mouse.c - mouse managing
+ *
+ * Copyright © 2007-2009 Julien Danjou <julien@danjou.info>
+ * Copyright © 2024 somewm contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
+
+/** Manipulate and inspect the mouse cursor.
+ *
+ * The mouse buttons are represented as index. The common ones are:
+ *
+ * ![Client geometry](../images/mouse.svg)
+ *
+ * It is possible to be notified of mouse events by connecting to various
+ * `client`, `widget`s and `wibox` signals:
+ *
+ *  * `mouse::enter`
+ *  * `mouse::leave`
+ *  * `mouse::press`
+ *  * `mouse::release`
+ *  * `mouse::move`
+ *
+ * See also `mousegrabber`
+ *
+ * @author Julien Danjou &lt;julien@danjou.info&gt;
+ * @copyright 2008-2009 Julien Danjou
+ * @inputmodule mouse
+ */
+
+#include "objects/mouse.h"
 
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
+#include <math.h>
 #include <string.h>
 
 #include "somewm_api.h"
 #include "somewm_types.h"
 #include "common/lualib.h"
+#include "common/util.h"
 #include "luaa.h"
-#include "objects/mouse.h"
+#include "globalconf.h"
 #include "objects/client.h"
+#include "objects/drawin.h"
 #include "objects/screen.h"
 
-/** Get or set mouse coordinates
- * \param L The Lua VM state
- * \return Number of elements pushed on stack
- *
- * Usage:
- *   local coords = mouse.coords()  -- Returns {x=X, y=Y, buttons={...}}
- *   mouse.coords({x=100, y=200})   -- Warp mouse to position
+static int miss_index_handler    = LUA_REFNIL;
+static int miss_newindex_handler = LUA_REFNIL;
+
+/**
+ * The `screen` under the cursor
+ * @property screen
+ * @tparam screen|nil screen
  */
+
+/** Get the pointer position.
+ * \param x will be set to the Pointer-x-coordinate relative to root
+ * \param y will be set to the Pointer-y-coordinate relative to root
+ * \param mask will be set to the current buttons state (5 buttons)
+ * \return true on success, false if an error occurred
+ **/
+bool
+mouse_query_pointer(int16_t *x, int16_t *y, uint16_t *mask)
+{
+    double dx, dy;
+    int button_states[5];
+
+    some_get_cursor_position(&dx, &dy);
+    *x = (int16_t)dx;
+    *y = (int16_t)dy;
+
+    if (mask) {
+        some_get_button_states(button_states);
+        /* Convert button array to X11-style mask for API compatibility */
+        *mask = 0;
+        for (int i = 0; i < 5; i++) {
+            if (button_states[i])
+                *mask |= (1 << (8 + i)); /* XCB_BUTTON_MASK_1 = 1<<8, etc. */
+        }
+    }
+
+    return true;
+}
+
+/** Get the pointer position on the screen.
+ * \param x This will be set to the Pointer-x-coordinate relative to root.
+ * \param y This will be set to the Pointer-y-coordinate relative to root.
+ * \param mask This will be set to the current buttons state.
+ * \return True on success, false if an error occurred.
+ */
+static bool
+mouse_query_pointer_root(int16_t *x, int16_t *y, uint16_t *mask)
+{
+    return mouse_query_pointer(x, y, mask);
+}
+
+/** Set the pointer position.
+ * \param x X-coordinate.
+ * \param y Y-coordinate.
+ */
+static inline void
+mouse_warp_pointer(int16_t x, int16_t y)
+{
+    some_set_cursor_position((double)x, (double)y, 0);
+}
+
+/** Push a table with mouse status.
+ * \param L The Lua VM state.
+ * \param x The x coordinate.
+ * \param y The y coordinate.
+ * \param mask The button mask.
+ */
+int
+luaA_mouse_pushstatus(lua_State *L, int x, int y, uint16_t mask)
+{
+    lua_createtable(L, 0, 3);
+    lua_pushinteger(L, x);
+    lua_setfield(L, -2, "x");
+    lua_pushinteger(L, y);
+    lua_setfield(L, -2, "y");
+
+    lua_createtable(L, 5, 0);
+
+    int i = 1;
+    /* Convert X11-style mask to button array */
+    for (uint16_t maski = (1 << 8); maski <= (1 << 12); maski <<= 1) {
+        if (mask & maski)
+            lua_pushboolean(L, true);
+        else
+            lua_pushboolean(L, false);
+        lua_rawseti(L, -2, i++);
+    }
+    lua_setfield(L, -2, "buttons");
+    return 1;
+}
+
+/* documented in lib/awful/mouse/init.lua */
 static int
 luaA_mouse_coords(lua_State *L)
 {
-	double x, y;
-	int button_states[5];
-	int i;
+    uint16_t mask;
+    int x, y;
+    int16_t mouse_x, mouse_y;
 
-	/* If table argument provided, set cursor position */
-	if (lua_gettop(L) >= 1 && lua_istable(L, 1)) {
-		lua_getfield(L, 1, "x");
-		lua_getfield(L, 1, "y");
+    if (lua_gettop(L) >= 1)
+    {
+        luaA_checktable(L, 1);
+        bool ignore_enter_notify = (lua_gettop(L) == 2 && luaA_checkboolean(L, 2));
 
-		if (lua_isnumber(L, -2) && lua_isnumber(L, -1)) {
-			x = lua_tonumber(L, -2);
-			y = lua_tonumber(L, -1);
+        if (!mouse_query_pointer_root(&mouse_x, &mouse_y, &mask))
+            return 0;
 
-			/* TODO: Handle 'silent' parameter */
-			some_set_cursor_position(x, y, 0);
-		}
+        x = round(luaA_getopt_number_range(L, 1, "x", mouse_x, MIN_X11_COORDINATE, MAX_X11_COORDINATE));
+        y = round(luaA_getopt_number_range(L, 1, "y", mouse_y, MIN_X11_COORDINATE, MAX_X11_COORDINATE));
 
-		lua_pop(L, 2); /* Pop x and y */
-	}
+        if (ignore_enter_notify)
+            client_ignore_enterleave_events();
 
-	/* Always return current position and button states */
-	some_get_cursor_position(&x, &y);
-	some_get_button_states(button_states);
+        mouse_warp_pointer(x, y);
 
-	/* Create return table {x=X, y=Y, buttons={1=bool, 2=bool, ...}} */
-	lua_newtable(L);
+        if (ignore_enter_notify)
+            client_restore_enterleave_events();
 
-	lua_pushnumber(L, x);
-	lua_setfield(L, -2, "x");
+        lua_pop(L, 1);
+    }
 
-	lua_pushnumber(L, y);
-	lua_setfield(L, -2, "y");
+    if (!mouse_query_pointer_root(&mouse_x, &mouse_y, &mask))
+        return 0;
 
-	/* Create buttons sub-table */
-	lua_newtable(L);
-	for (i = 0; i < 5; i++) {
-		lua_pushboolean(L, button_states[i]);
-		lua_rawseti(L, -2, i + 1); /* Lua uses 1-based indexing */
-	}
-	lua_setfield(L, -2, "buttons");
-
-	return 1;
+    return luaA_mouse_pushstatus(L, mouse_x, mouse_y, mask);
 }
 
-/** Get object under mouse pointer
- * \param L The Lua VM state
- * \return Number of elements pushed on stack
+/** Get the client or any object which is under the pointer.
  *
- * Usage:
- *   local obj = mouse.object_under_pointer()  -- Returns client or nil
+ * @treturn client|wibox|nil A client, wibox or nil.
+ * @staticfct object_under_pointer
  */
 static int
 luaA_mouse_object_under_pointer(lua_State *L)
 {
-	Client *c = some_object_under_cursor();
-	drawin_t *d;
+    int16_t mouse_x, mouse_y;
 
-	if (c) {
-		/* Push client object */
-		luaA_object_push(L, c);
-		return 1;
-	}
+    if (!mouse_query_pointer_root(&mouse_x, &mouse_y, NULL))
+        return 0;
 
-	/* Check for drawin (wibox) under cursor */
-	d = some_drawin_under_cursor();
-	if (d) {
-		/* Push drawin object */
-		luaA_object_push(L, d);
-		return 1;
-	}
+    drawin_t *drawin;
+    client_t *client;
 
-	/* Nothing under cursor */
-	lua_pushnil(L);
-	return 1;
+    if ((drawin = some_drawin_under_cursor()))
+        return luaA_object_push(L, drawin);
+
+    if ((client = some_object_under_cursor()))
+        return luaA_object_push(L, client);
+
+    return 0;
 }
 
-/** Property getter for mouse object
- * \param L The Lua VM state
- * \return Number of elements pushed on stack
- *
- * Handles: mouse.screen
+/** Mouse library.
+ * \param L The Lua VM state.
+ * \return The number of elements pushed on stack.
+ * \luastack
+ * \lfield coords Mouse coordinates.
+ * \lfield screen Mouse screen.
  */
 static int
 luaA_mouse_index(lua_State *L)
 {
-	const char *prop;
-	Monitor *m;
+    const char *attr = luaL_checkstring(L, 2);
+    int16_t mouse_x, mouse_y;
 
-	/* Check if accessing a method first */
-	lua_getmetatable(L, 1);
-	lua_pushvalue(L, 2);
-	lua_rawget(L, -2);
+    /* attr is not "screen"?! */
+    if (A_STRNEQ(attr, "screen")) {
+        if (miss_index_handler != LUA_REFNIL) {
+            return luaA_call_handler(L, miss_index_handler);
+        }
+        else
+            return luaA_default_index(L);
+    }
 
-	if (!lua_isnil(L, -1))
-		return 1;
+    if (!mouse_query_pointer_root(&mouse_x, &mouse_y, NULL))
+    {
+        /* Nothing ever handles mouse.screen being nil. Lying is better than
+         * having lots of lua errors in this case.
+         */
+        if (globalconf.focus.client)
+            luaA_object_push(L, globalconf.focus.client->screen);
+        else
+            luaA_object_push(L, luaA_screen_get_primary_screen(L));
+        return 1;
+    }
 
-	lua_pop(L, 2);
-
-	/* Handle property access */
-	if (lua_type(L, 2) != LUA_TSTRING)
-		return 0;
-
-	prop = lua_tostring(L, 2);
-
-	if (strcmp(prop, "screen") == 0) {
-		/* Return screen under cursor */
-		screen_t *screen;
-
-		m = some_monitor_at_cursor();
-		if (m) {
-			/* Convert Monitor* to screen_t* object */
-			screen = luaA_screen_get_by_monitor(L, m);
-			if (screen && screen->valid) {
-				luaA_screen_push(L, screen);
-				return 1;
-			}
-		}
-
-		/* Fallback: Return primary screen (never return nil) */
-		screen = luaA_screen_get_primary_screen(L);
-		if (screen && screen->valid) {
-			luaA_screen_push(L, screen);
-			return 1;
-		}
-
-		/* Last resort: should never happen in normal operation */
-		fprintf(stderr, "somewm: WARNING: mouse.screen has no valid screens available\n");
-		lua_pushnil(L);
-		return 1;
-	}
-
-	return 0;
+    luaA_object_push(L, luaA_screen_getbycoord(L, mouse_x, mouse_y));
+    return 1;
 }
 
-/** Property setter for mouse object
- * \param L The Lua VM state
- * \return Number of elements pushed on stack
- *
- * Handles: mouse.screen = s
+/** Newindex for mouse.
+ * \param L The Lua VM state.
+ * \return The number of elements pushed on stack.
  */
 static int
 luaA_mouse_newindex(lua_State *L)
 {
-	const char *prop;
-	Monitor *m;
+    const char *attr = luaL_checkstring(L, 2);
+    screen_t *screen;
 
-	if (lua_type(L, 2) != LUA_TSTRING)
-		return 0;
+    if (A_STRNEQ(attr, "screen")) {
+        /* Call the lua mouse property handler */
+        if (miss_newindex_handler != LUA_REFNIL) {
+            return luaA_call_handler(L, miss_newindex_handler);
+        }
+        else
+            return luaA_default_newindex(L);
+    }
 
-	prop = lua_tostring(L, 2);
-
-	if (strcmp(prop, "screen") == 0) {
-		/* Warp cursor to specified screen */
-		m = (Monitor *)lua_touserdata(L, 3);
-		if (m) {
-			some_warp_cursor_to_monitor(m);
-		}
-		return 0;
-	}
-
-	return 0;
+    screen = luaA_checkscreen(L, 3);
+    mouse_warp_pointer(screen->geometry.x, screen->geometry.y);
+    return 0;
 }
 
-/** AwesomeWM compatibility: mouse.set_index_miss_handler */
+/**
+ * Add a custom property handler (getter).
+ * @staticfct set_index_miss_handler
+ */
 static int
 luaA_mouse_set_index_miss_handler(lua_State *L)
 {
-	return luaA_registerfct(L, 1, &mouse_handlers.index_miss_handler);
+    return luaA_registerfct(L, 1, &miss_index_handler);
 }
 
-/** AwesomeWM compatibility: mouse.set_newindex_miss_handler */
+/**
+ * Add a custom property handler (setter).
+ * @staticfct set_newindex_miss_handler
+ */
 static int
 luaA_mouse_set_newindex_miss_handler(lua_State *L)
 {
-	return luaA_registerfct(L, 1, &mouse_handlers.newindex_miss_handler);
+    return luaA_registerfct(L, 1, &miss_newindex_handler);
 }
 
-/* Mouse object methods table */
-static const luaL_Reg mouse_methods[] = {
-	{ "__index", luaA_mouse_index },
-	{ "__newindex", luaA_mouse_newindex },
-	{ "coords", luaA_mouse_coords },
-	{ "object_under_pointer", luaA_mouse_object_under_pointer },
-	{ "set_index_miss_handler", luaA_mouse_set_index_miss_handler },
-	{ "set_newindex_miss_handler", luaA_mouse_set_newindex_miss_handler },
-	{ NULL, NULL }
+const struct luaL_Reg awesome_mouse_methods[] =
+{
+    { "__index", luaA_mouse_index },
+    { "__newindex", luaA_mouse_newindex },
+    { "coords", luaA_mouse_coords },
+    { "object_under_pointer", luaA_mouse_object_under_pointer },
+    { "set_index_miss_handler", luaA_mouse_set_index_miss_handler},
+    { "set_newindex_miss_handler", luaA_mouse_set_newindex_miss_handler},
+    { NULL, NULL }
 };
 
-/** Initialize the mouse object for Lua
- * Creates global 'mouse' object and capi.mouse
+const struct luaL_Reg awesome_mouse_meta[] =
+{
+    { NULL, NULL }
+};
+
+/** Initialize the mouse object for Lua.
+ * Creates global 'mouse' object using AwesomeWM's luaA_openlib pattern.
  */
 void
 luaA_mouse_setup(lua_State *L)
 {
-	/* Get or create capi table */
-	lua_getglobal(L, "capi");
-	if (lua_isnil(L, -1)) {
-		lua_pop(L, 1);
-		lua_newtable(L);
-		lua_pushvalue(L, -1);
-		lua_setglobal(L, "capi");
-	}
-
-	/* Create the mouse object */
-	lua_newtable(L);
-
-	/* Set metatable */
-	lua_newtable(L);
-	luaA_setfuncs(L, mouse_methods);
-	lua_setmetatable(L, -2);
-
-	/* Duplicate the mouse table */
-	lua_pushvalue(L, -1);
-
-	/* Set capi.mouse = mouse_table */
-	lua_setfield(L, -3, "mouse");
-
-	/* Also set global mouse for compatibility */
-	lua_setglobal(L, "mouse");
-
-	lua_pop(L, 1);  /* Pop capi table */
+    luaA_openlib(L, "mouse", awesome_mouse_methods, awesome_mouse_meta);
 }
+
+// vim: filetype=c:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
--- objects/mouse.h
+++ objects/mouse.h
@@ -1,12 +1,36 @@
 /*
- * mouse.h - Mouse object bindings for Lua
+ * mouse.h - mouse managing header
+ *
+ * Copyright © 2007-2008 Julien Danjou <julien@danjou.info>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
-#ifndef SOMEWM_MOUSE_H
-#define SOMEWM_MOUSE_H
+#ifndef AWESOME_MOUSE_H
+#define AWESOME_MOUSE_H
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <lua.h>
 
+bool mouse_query_pointer(int16_t *, int16_t *, uint16_t *);
+int luaA_mouse_pushstatus(lua_State *, int, int, uint16_t);
+
+/* somewm-specific setup function */
 void luaA_mouse_setup(lua_State *L);
 
-#endif /* SOMEWM_MOUSE_H */
+#endif
+// vim: filetype=c:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
