7e092ad7eedd7650cea1e5b3ea4befeb6e4ac8d3
Jimmy Cozza <moatcozza@gmail.com>
2026-01-06 23:32:55 -0700

    api: align selection_acquire.c symbols with AwesomeWM
    Add missing X11 stubs for API parity:
    - Add selection_handle_selectionclear() stub
    - Add selection_handle_selectionrequest() stub
    - Add xcb/xcb.h include for X11 types

--- objects/selection_acquire.h
+++ objects/selection_acquire.h
@@ -24,8 +24,11 @@
 #define SOMEWM_OBJECTS_SELECTION_ACQUIRE_H
 
 #include <lua.h>
+#include <xcb/xcb.h>
 
-void selection_acquire_class_setup(lua_State *);
+void selection_acquire_class_setup(lua_State*);
+void selection_handle_selectionclear(xcb_selection_clear_event_t*);
+void selection_handle_selectionrequest(xcb_selection_request_event_t*);
 
 #endif
 
