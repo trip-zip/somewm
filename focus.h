9e3dfcc11da541b55765fc9572837b1919b3c911
Jimmy Cozza <moatcozza@gmail.com>
2026-04-10 12:26:07 -0600

    refactor: privatize module-local state
    Move globals that are only used within a single extracted module from
    somewm.h (visible to all modules) to static definitions in their
    owning module:
    
    input.c: cursor_mode, gesture_swipe_consumed, gesture_pinch_consumed,
             gesture_hold_consumed
    monitor.c: in_updatemons, updatemons_pending
    
    Remove some_update_pointer_constraint entirely. It was declared in
    both focus.h and somewm_api.h but never called from outside focus.c.
    The constraint update is done inline in focusclient() via cursorconstrain().
    
    somewm.h drops from 40 to 33 extern declarations.

--- focus.h
+++ focus.h
@@ -8,11 +8,8 @@
 
 #include "somewm_types.h"
 
-struct wlr_surface;
-
 void focusclient(Client *c, int lift);
 Client *focustop(Monitor *m);
 void focus_restore(Monitor *m);
-void some_update_pointer_constraint(struct wlr_surface *surface);
 
 #endif /* FOCUS_H */
