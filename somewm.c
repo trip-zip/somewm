9bc8b0ea2b3e1f92495d8cc3669de954f079433e
Stefan Huber <shuber@sthu.org>
2026-04-13 11:48:11 +0200

    fix: Assert upon monitor unplug due to missing surface cleanup #442
    When a zwlr_layer_surface_v1 is created in createsurfacelayer() then
    a wlr_surface_send_enter() is unconditionally fired for the created
    surface.
    
    Consequently, wlr_surface_send_leave() need also be sent prior
    destroying them. Not doing so (apparently) leaves listeners registered,
    e.g., for output->events.bind or others, eventually causing an assert()
    in wlr_output_finish().
    
    This leads to crashes during updatemon() when a screen is removed, e.g.,
    when unplugging external monitors, see issue #442.
    
    Add wlr_surface_send_leave() in unmaplayersurfacenotify() to fix #442.

--- somewm.c
+++ somewm.c
@@ -5870,6 +5870,9 @@ unmaplayersurfacenotify(struct wl_listener *listener, void *data)
 	if (l == exclusive_focus)
 		exclusive_focus = NULL;
 
+	if (l->layer_surface && l->layer_surface->surface && l->layer_surface->output)
+		wlr_surface_send_leave(l->layer_surface->surface, l->layer_surface->output);
+
 	if (l->layer_surface->output && (l->mon = l->layer_surface->output->data))
 		arrangelayers(l->mon);
 
